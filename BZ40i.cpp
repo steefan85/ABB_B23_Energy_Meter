e273b038649ac347b3ca48825b15250b2f7452c3
Florian Knodt <git@adlerweb.info>
2021-07-07 01:39:50 +0200

    Fix broken unicode comments

--- BZ40i.cpp
+++ BZ40i.cpp
@@ -1,8 +1,8 @@
 /* Library for reading Berg BZ40i Modbus Energy meters.
 *  Reading via Hardware or Software Serial library & rs232<->rs485 converter
-*  2019 Florian "adlerweb" Knodt � www.adlerweb.info
+*  2019 Florian "adlerweb" Knodt · www.adlerweb.info
 *  Based on SDM_Energy_Meter 2016-2019 Reaper7
-*  crc calculation by Jaime Garc�a (https://github.com/peninquen/Modbus-Energy-Monitor-Arduino/)
+*  crc calculation by Jaime García (https://github.com/peninquen/Modbus-Energy-Monitor-Arduino/)
 */
 //------------------------------------------------------------------------------
 #include "BZ40i.h"
